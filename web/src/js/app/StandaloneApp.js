83bddf582e250ebbb344da891cde22c38097f340
Zach Mullen <zach.mullen@kitware.com>
2014-08-25 15:45:20 -0400

    Working close buttons on control panel, control panel refactor

--- web/src/js/app/StandaloneApp.js
+++ web/src/js/app/StandaloneApp.js
@@ -1,4 +1,10 @@
 cinema.StandaloneApp = Backbone.View.extend({
+    events: {
+        'click .c-control-panel-header a.close': function (e) {
+            $(e.currentTarget).parents('.c-control-panel').fadeOut();
+        }
+    },
+
     initialize: function (settings) {
         this.dataRoot = settings.dataRoot;
         this.staticRoot = settings.staticRoot;
