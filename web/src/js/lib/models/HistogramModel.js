dd05dd409b82c25efb280d02a2d3c6c4e08424b0
Zach Mullen <zach.mullen@kitware.com>
2014-09-17 16:24:23 -0600

    Add histogram model, change data structure in layer model

---
+++ web/src/js/lib/models/HistogramModel.js
@@ -0,0 +1,22 @@
+/**
+ * This model holds the histogram data for a particular layer combination.
+ */
+
+cinema.models.HistogramModel = Backbone.Model.extend({
+    constructor: function (settings) {
+        Backbone.Model.apply(this, arguments);
+
+        this.layerModel = settings.layerModel;
+        this.basePath = settings.basePath;
+    },
+
+    fetch: function () {
+        var layerStr = _.keys(this.layerModel.get('state')).sort().join('/');
+
+        if (!layerStr) {
+            return;
+        }
+        this.url = this.basePath + '/layers/' + layerStr + '/histogram.json';
+        return Backbone.Model.prototype.fetch.apply(this, arguments);
+    }
+});
