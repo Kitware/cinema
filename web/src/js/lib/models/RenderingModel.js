4d3ea9718677595c268d95c480f151cbc82a38b5
Scott Wittenburg <scott.wittenburg@kitware.com>
2014-10-14 09:00:44 -0600

    Fixed bug in clamping where interior control points weren't respaced.

--- web/src/js/lib/models/RenderingModel.js
+++ web/src/js/lib/models/RenderingModel.js
@@ -122,20 +122,42 @@ cinema.models.RenderingModel = Backbone.Model.extend({
             controlPoints = $.extend(true, [], config.controlPoints),
             clampedRange = config.clampedRange,
             dataRange = config.dataRange,
-            currentControlIdx = 0;
+            currentControlIdx = 0,
+            needRemap = false,
+            minIdx = 0,
+            maxIdx = 0;
 
+        // If there is a min clamp, insert a new control point and mark that remap is needed
         if (clampedRange[0] > dataRange[0]) {
             var cp1 = controlPoints[0];
             var frac1 = (clampedRange[0] - dataRange[0]) / (dataRange[1] - dataRange[0]);
             controlPoints.splice(1, 0, {'r': cp1.r, 'g': cp1.g, 'b': cp1.b, 'x': frac1});
+            needRemap = true;
+            minIdx = 1;
         }
 
+        // Ditto if there is a max clamp
         if (clampedRange[1] < dataRange[1]) {
             var cpn = controlPoints[controlPoints.length - 1];
             var fracn = (clampedRange[1] - dataRange[0]) / (dataRange[1] - dataRange[0]);
             controlPoints.splice(controlPoints.length - 1, 0, {'r': cpn.r, 'g': cpn.g, 'b': cpn.b, 'x': fracn});
+            needRemap = true;
+            maxIdx = controlPoints.length - 2;
+        } else {
+            maxIdx = controlPoints.length - 1;
         }
 
+        // Now remap the interior control points so they have the same spacing
+        // as before the clamp.
+        if (needRemap === true) {
+            var span = controlPoints[maxIdx].x - controlPoints[minIdx].x;
+            for (var i = minIdx + 1; i < maxIdx; i += 1) {
+                controlPoints[i].x = (controlPoints[i].x * span) + controlPoints[minIdx].x;
+            }
+        }
+
+        // Now actually generate the lookup table from the (maybe modified)
+        // control points.
         for (var idx = 0; idx < 256; idx += 1) {
             var value = idx / 255.0,
                 pointA = this.extractPoint(controlPoints, currentControlIdx),
