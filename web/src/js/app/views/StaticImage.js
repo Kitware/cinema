70f404a85ba328b7ae7506a2b4c16b88ca02b8ec
Scott Wittenburg <scott.wittenburg@kitware.com>
2014-10-24 15:20:38 -0600

    Add information widget composed of two subwidgets, still only for static search
    Going forward we can make all views involving search/histogram views use this
    widget, and dynamically choose what kind of widget they want for the top part
    (at the moment, we have only pipeline or static information).

--- web/src/js/app/views/StaticImage.js
+++ web/src/js/app/views/StaticImage.js
@@ -12,7 +12,9 @@
             dataType = model.getDataType(),
             controlModel = new cinema.models.ControlModel({ info: model }),
             viewpointModel = new cinema.models.ViewPointModel({ controlModel: controlModel }),
-            histogramModel = new cinema.models.StaticHistogramModel({ basePath: model.get('basePath') }),
+            histogramModel = (_.has(model.attributes.metadata, 'analysis') === false ? null :
+                new cinema.models.StaticHistogramModel({ basePath: model.get('basePath'),
+                                                         analysisInfo: model.attributes.metadata.analysis })),
             renderer = new cinema.views.StaticImageVisualizationCanvasWidget({
                 el: $('.c-body-container', container),
                 model: model,
@@ -39,31 +41,50 @@
                 toolbarSelector: '.c-panel-toolbar',
                 toolbarRootView: fakeToolbarRootView
             }),
-            staticHistogram = new cinema.views.StaticHistogramWidget({
-                el: $('.c-static-histogram-panel', container),
-                basePath: model.get('basePath'),
-                histogramModel: histogramModel,
-                viewpoint: viewpointModel,
-                controlModel: controlModel,
-                visModel: model,
-                toolbarSelector: '.c-panel-toolbar'
-            }),
+            staticHistogram = (_.has(model.attributes.metadata, 'analysis') === false ? null :
+                new cinema.views.StaticHistogramWidget({
+                    el: $('.c-static-histogram-panel', container),
+                    basePath: model.get('basePath'),
+                    histogramModel: histogramModel,
+                    viewpoint: viewpointModel,
+                    controlModel: controlModel,
+                    visModel: model,
+                    toolbarSelector: '.c-panel-toolbar'
+                })),
+            searchInformation = (_.has(model.attributes.metadata, 'analysis') === false ? null :
+                new cinema.views.ComposableInformationWidget({
+                    el: $('.c-information-panel', container),
+                    model: model,
+                    controlModel: controlModel,
+                    exclude: ['layer', 'filename'],
+                    // layers: layerModel,
+                    analysisInfo: model.attributes.metadata.analysis,
+                    toolbarSelector: '.c-panel-toolbar'
+                })),
             controlList = [
-                { position: 'right', key: 'tools', icon: 'icon-tools', title: 'Tools' },
-                { position: 'center', key: 'static-histogram', icon: 'icon-chart-bar', title: 'Histogram' }
+                { position: 'right', key: 'tools', icon: 'icon-tools', title: 'Tools' }
             ],
             firstRender = true;
+
+            if (_.has(model.attributes.metadata, 'analysis')) {
+                controlList.unshift({ position: 'left', key: 'information', icon: 'icon-help', title: 'Information' });
+                controlList.unshift({ position: 'center', key: 'static-histogram', icon: 'icon-chart-bar', title: 'Histogram' });
+            }
 
             function render () {
                 var root = $(rootSelector);
                 fakeToolbarRootView.update(root);
                 renderer.setElement($('.c-body-container', root)).render();
                 controlTools.setElement($('.c-tools-panel', root)).render();
-                staticHistogram.setElement($('.c-static-histogram-panel', root)).render();
+                if (_.has(model.attributes.metadata, 'analysis')) {
+                    staticHistogram.setElement($('.c-static-histogram-panel', root)).render();
+                    searchInformation.setElement($('.c-information-panel', root)).render();
+                }
                 refreshCamera(true);
                 if (firstRender) {
                     firstRender = false;
                     $('.c-static-histogram-panel', root).hide();
+                    $('.c-information-panel', root).hide();
                 }
             }
 
