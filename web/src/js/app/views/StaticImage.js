8671da777c8eff85338aa7bd881b7ccfcf30fdef
Scott Wittenburg <scott.wittenburg@kitware.com>
2014-11-11 15:36:52 -0700

    Cause composite view to fetch histogram on initialization.

--- web/src/js/app/views/StaticImage.js
+++ web/src/js/app/views/StaticImage.js
@@ -146,8 +146,8 @@
                 this.controlList.push(
                     { position: 'center', key: 'static-histogram', icon: 'icon-chart-bar', title: 'Histogram' }
                 );
-                this.histogramModel = sharedData.histogram;
-                this.histogramModel.fetch( { 'controlModel': this.controlModel } );
+
+                sharedData.histogram.fetch( { 'controlModel': this.controlModel } );
             } else {
                 this.searchInformation = sharedData.metaDataInformationWidget;
             }
