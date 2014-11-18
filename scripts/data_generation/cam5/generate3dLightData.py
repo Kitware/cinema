97d895f755b3cc70da18375d5c4e2de9886f63b7
Scott Wittenburg <scott.wittenburg@kitware.com>
2014-11-18 08:34:15 -0700

    Get the cam5 scripts for dynamic rendering and static volumes working.

--- scripts/data_generation/cam5/generate3dLightData.py
+++ scripts/data_generation/cam5/generate3dLightData.py
@@ -22,7 +22,7 @@
 # ParaView Python - Path setup
 # -----------------------------------------------------------------------------
 
-import sys, os
+import sys, os, argparse
 
 from paraview.simple import *
 from paraview import data_exploration as wx
@@ -44,201 +44,209 @@ def updateGlobalRange(nextRange, globalRange):
 
     return newGlobal
 
-# -----------------------------------------------------------------------------
-# Configuration
-# -----------------------------------------------------------------------------
 
-data_to_process = {
-    "temperature": {
-        "scalarRange": [5.0, 25.0],
-        "contours": [5.0, 10.0, 15.0, 20.0, 25.0],
+def doProcessing(outputDir, timesteps):
+    # -----------------------------------------------------------------------------
+    # Configuration
+    # -----------------------------------------------------------------------------
+
+    data_to_process = {
+        "T": {
+            "scalarRange": [178, 312],
+            "contours": [ 265, 275, 285, 295 ]
+        }
     }
-}
 
-#resolution = 500
-resolution = 500
-#phis = [ float(r) for r in range(0, 360, 90)]
-#thetas = [ 0.0, 30.0 ]
-phis = [ float(r) for r in range(0, 360, 20)]
-thetas = [-80.0, -60.0, -30.0, 0.0, 30.0, 60.0, 80.0]
-distance = 25000000.0
+    #resolution = 500
+    resolution = 500
+    #phis = [ 90, 180 ]
+    #thetas = [ 0.0 ]
+    phis = [ float(r) for r in range(0, 360, 20)]
+    thetas = [-80.0, -60.0, -30.0, 0.0, 30.0, 60.0, 80.0]
+    distance = 450
+    rotation_axis = [0.0, 0.0, 1.0]
+    center_of_rotation = [0.0, 0.0, 0.0]
 
-# -----------------------------------------------------------------------------
-# Input data definition
-# -----------------------------------------------------------------------------
+    # -----------------------------------------------------------------------------
+    # Input data definition
+    # -----------------------------------------------------------------------------
 
-#path_root = '/Volumes/OLeary'
-path_root = '/media/scott/CINEMA FAT/DataExploration'
+    #path_root = '/Volumes/OLeary'
+    path_root = '/media/scott/CINEMA FAT'
 
-data_base_path = os.path.join(path_root, 'Data/MPAS/data')
-earth_core_path = os.path.join(data_base_path, 'earth-high.vtk')
+    data_base_path = os.path.join(path_root, 'cam5')
+    earth_core_path = os.path.join(data_base_path, 'earth-high.vtk')
 
-#output_working_dir = os.path.join(path_root, 'Output/MPAS/web-generated/mpas-composite')
-output_working_dir = '/home/scott/Documents/cinemaDemo/simpleCinemaWebGL/mpas-data/mpas-composite-light'
+    #output_working_dir = os.path.join(path_root, 'Output/MPAS/web-generated/mpas-composite')
+    #output_working_dir = '/home/scott/Documents/cinemaDemo/simpleCinemaWebGL/cam5/contour-webgl-light'
+    output_working_dir = os.path.join(outputDir, 'contour-webgl-light')
 
-globe_file_pattern = 'xyz_n_primal/X_Y_Z_NLAYER-primal_%d_0.vtu'
+    earth_file = os.path.join(data_base_path, 'earth.vtk')
+    file_pattern = os.path.join(data_base_path, 'cam5earth_%d.vtk')
+    #file_times = range(0, 13, 1)
+    #file_times = [ 6 ]
+    file_times = timesteps
+    cam5_filenames = [ (file_pattern % int(time)) for time in file_times]
 
-# globe_file_times = range(50, 51, 50) # 1 timesteps
-# globe_file_times = range(50, 101, 50) # 2 timesteps
-# globe_file_times = range(50, 501, 50) # 10 timesteps
-# globe_file_times = range(50, 5151, 500) # 10 timesteps for 100th steps
-globe_file_times = range(50, 5151, 50) # all timesteps
+    # -----------------------------------------------------------------------------
+    # Pipeline definition
+    # -----------------------------------------------------------------------------
 
-deltaTimeStep = 10
+    cam5earth   = LegacyVTKReader(FileNames=cam5_filenames)
 
-globe_filenames = [ os.path.join(data_base_path, (globe_file_pattern % time)) for time in globe_file_times]
+    earthvtk    = LegacyVTKReader(FileNames=[earth_file])
+    extract_globe_surface = ExtractSurface( Input = earthvtk )
+    surface_normals = GenerateSurfaceNormals( Input = extract_globe_surface )
 
-# -----------------------------------------------------------------------------
-# Pipeline definition
-# -----------------------------------------------------------------------------
+    # Generate iso contours
+    contours = {}
+    for field in data_to_process:
+        contours[field] = []
+        for isoValue in data_to_process[field]['contours']:
+            contours[field].append(Contour(Input = cam5earth,
+                                           PointMergeMethod = "Uniform Binning",
+                                           ContourBy = field,
+                                           Isosurfaces = [isoValue],
+                                           ComputeScalars = 1))
 
-# Globe Core
-globe_core = LegacyVTKReader(FileNames=[earth_core_path])
-extract_globe_surface = ExtractSurface( Input = globe_core )
-surface_normals = GenerateSurfaceNormals( Input = extract_globe_surface )
+    # -----------------------------------------------------------------------------
+    # Output configuration
+    # -----------------------------------------------------------------------------
 
-# Data pipeline
-globe_reader    = XMLUnstructuredGridReader(FileName = globe_filenames,
-                                            CellArrayStatus = ['temperature', 'salinity', 'density', 'pressure'])
-globe_threshold = Threshold(Input = globe_reader,
-                            Scalars = ['CELLS', 'temperature'],
-                            ThresholdRange = [-1000.0, 50.0])
-data_to_points  = CellDatatoPointData(Input = globe_threshold)
+    title       = "Visualizations from Cam5 Data"
+    description = """
+                  Atmospheric Simulations
+                  """
 
-# Generate iso contours
-contours = {}
-for field in data_to_process:
-    contours[field] = []
-    for isoValue in data_to_process[field]['contours']:
-        contours[field].append(Contour(Input = data_to_points,
-                                       PointMergeMethod = "Uniform Binning",
-                                       ContourBy = field,
-                                       Isosurfaces = [isoValue],
-                                       ComputeScalars = 1))
+    analysis = wx.AnalysisManager( output_working_dir, title, description,
+                                   author="Patrick O'Leary",
+                                   code_name="cam5",
+                                   code_version="N/A",
+                                   cores=1)
 
-# -----------------------------------------------------------------------------
-# Output configuration
-# -----------------------------------------------------------------------------
+    # -----------------------------------------------------------------------------
+    # Pre-calculate ranges
+    # -----------------------------------------------------------------------------
 
-title       = "MPAS - World Ocean - 120km"
-description = """
-              The following data anaylisis tries to simulate the evolution
-              of the ocean in terms of temperature and salinity distribution accross
-              """
+    globalRanges = {
+        'T': [ 10000000, -10000000 ],
+        'U': [ 10000000, -10000000 ],
+        'V': [ 10000000, -10000000 ],
+        'magnitude': [ 10000000, -10000000 ]
+    }
 
-analysis = wx.AnalysisManager( output_working_dir, title, description,
-                               author="Andy Bauer",
-                               code_name="MPAS",
-                               code_version="1.3 + Ben changes",
-                               cores=1)
+    print 'Calculating data array ranges over time'
+    for time in file_times:
+        t = int(time)
+        print '  timestep ',t
+        cam5earth.UpdatePipeline(t)
+        pdi = cam5earth.GetPointDataInformation()
 
-# -----------------------------------------------------------------------------
-# Pre-calculate ranges
-# -----------------------------------------------------------------------------
+        for key in globalRanges.keys():
+            globalRanges[key] = updateGlobalRange(pdi.GetArray(key).GetRange(), globalRanges[key])
 
-globalRanges = {
-    'temperature': [ 10000000, -10000000 ],
-    'salinity': [ 10000000, -10000000 ],
-    'density': [ 10000000, -10000000 ],
-    'pressure': [ 10000000, -10000000 ]
-}
+    print 'Discovered global ranges:'
+    print globalRanges
 
-for t in range(0, len(globe_file_times), deltaTimeStep):
-    print '  timestep ',t
-    globe_threshold.UpdatePipeline(t)
-    cdi = globe_threshold.GetCellDataInformation()
-    #data_to_points.UpdatePipeline(t)
-    #pdi = data_to_points.GetPointDataInformation()
+    # -----------------------------------------------------------------------------
+    # Composite generator
+    # -----------------------------------------------------------------------------
 
-    for key in globalRanges.keys():
-        globalRanges[key] = updateGlobalRange(cdi.GetArray(key).GetRange(), globalRanges[key])
-        #globalRanges[key] = updateGlobalRange(pdi.GetArray(key).GetRange(), globalRanges[key])
+    id = 'contours-lit'
+    title = 'Lit 3D contours composite'
+    description = '3D contour of temperature'
+    analysis.register_analysis(id, title, description, '{time}/{theta}/{phi}/{filename}', wx.CompositeImageExporter.get_data_type())
+    fng = analysis.get_file_name_generator(id)
 
-print 'Discovered global ranges:'
-print globalRanges
+    camera_handler = wx.ThreeSixtyCameraHandler(fng,
+                                                None,
+                                                phis,
+                                                thetas,
+                                                center_of_rotation,
+                                                rotation_axis,
+                                                distance)
 
-# -----------------------------------------------------------------------------
-# Composite generator
-# -----------------------------------------------------------------------------
-
-id = 'iso-composite-' + str(resolution)
-title = '3D contours composite'
-description = '3D contour of temperature and salinity'
-analysis.register_analysis(id, title, description, '{time}/{theta}/{phi}/{filename}', wx.CompositeImageExporter.get_data_type())
-fng = analysis.get_file_name_generator(id)
-
-camera_handler = wx.ThreeSixtyCameraHandler(fng, None, phis, thetas, [0.0, 0.0, 0.0], [0.0, 0.0, 1.0], distance)
-
-iso_color_array = [
-    ('VALUE', 'temperature'),
-    ('VALUE', 'salinity'),
-    ('VALUE', 'pressure'),
-    ('VALUE', 'density'),
-    ('VALUE', 'nX'),
-    ('VALUE', 'nY'),
-    ('VALUE', 'nZ')
-]
-
-pdi = globe_core.GetPointDataInformation()
-bottomDepthRange = pdi.GetArray('bottomDepth').GetRange()
-print 'bottomDepth range:',bottomDepthRange
-
-luts = {
-    "temperature": ["point", "temperature", 0, globalRanges['temperature']],
-    "salinity": ["point", "salinity", 0, globalRanges['salinity']],
-    "pressure": ["point", "pressure", 0, globalRanges['pressure']],
-    "density": ["point", "density", 0, globalRanges['density']],
-    "bottomDepth": ["point", "bottomDepth", 0, bottomDepthRange],
-    'nX': ['point', 'Normals', 0, (-1,1)],
-    'nY': ['point', 'Normals', 1, (-1,1)],
-    'nZ': ['point', 'Normals', 2, (-1,1)]
-}
-
-composite_list = [ surface_normals ]
-composite_description = [{'name': 'Earth core'} ]
-colors = [
-    [
-        ('VALUE', 'bottomDepth'),
+    iso_color_array = [
+        ('VALUE', 'T'),
+        ('VALUE', 'U'),
+        ('VALUE', 'V'),
+        ('VALUE', 'magnitude'),
         ('VALUE', 'nX'),
         ('VALUE', 'nY'),
         ('VALUE', 'nZ')
     ]
-]
 
-#composite_list = []
-#composite_description = []
-#colors = []
+    luts = {
+        "T": ["point", "T", 0, globalRanges['T']],
+        "U": ["point", "U", 0, globalRanges['U']],
+        "V": ["point", "V", 0, globalRanges['V']],
+        "magnitude": ["point", "magnitude", 0, globalRanges['magnitude']],
+        "bottomDepth": ["point", "bottomDepth", 0, [-10277.0, 7170.0]],
+        'nX': ['point', 'Normals', 0, (-1,1)],
+        'nY': ['point', 'Normals', 1, (-1,1)],
+        'nZ': ['point', 'Normals', 2, (-1,1)]
+    }
 
-for field in contours:
-    for iso in contours[field]:
-        composite_description.append({'name': field[0] + "=" + str(iso.Isosurfaces[0]), 'parent': "Contour by %s" % field})
-        composite_list.append(iso)
-        colors.append(iso_color_array)
+    composite_list = [ surface_normals ]
+    composite_description = [{'name': 'Earth core'} ]
+    colors = [
+        [
+            ('VALUE', 'bottomDepth'),
+            ('VALUE', 'nX'),
+            ('VALUE', 'nY'),
+            ('VALUE', 'nZ')
+        ]
+    ]
 
-exporter = wx.CompositeImageExporter(fng,
-                                     composite_list,
-                                     colors,
-                                     luts,
-                                     camera_handler,
-                                     [resolution,resolution],
-                                     composite_description,
-                                     format='png') #, 0, 0)
+    for field in contours:
+        for iso in contours[field]:
+            composite_description.append({'name': field[0] + "=" + str(iso.Isosurfaces[0]), 'parent': "Contour by %s" % field})
+            composite_list.append(iso)
+            colors.append(iso_color_array)
 
-# Customize some view properties
-exporter.view.Background = [1.0, 1.0, 1.0]
-exporter.view.OrientationAxesVisibility = 0
-exporter.view.CenterAxesVisibility = 0
+    exporter = wx.CompositeImageExporter(fng,
+                                         composite_list,
+                                         colors,
+                                         luts,
+                                         camera_handler,
+                                         [resolution,resolution],
+                                         composite_description,
+                                         format='png') #, 0, 0)
 
-exporter.set_analysis(analysis)
+    # Customize some view properties
+    exporter.view.Background = [1.0, 1.0, 1.0]
+    exporter.view.OrientationAxesVisibility = 0
+    exporter.view.CenterAxesVisibility = 0
 
-# -----------------------------------------------------------------------------
-# Perform analysis
-# -----------------------------------------------------------------------------
-analysis.begin()
+    exporter.set_analysis(analysis)
 
-for time in range(0, len(globe_filenames), deltaTimeStep):
-    GetAnimationScene().TimeKeeper.Time = float(time)
-    fng.update_active_arguments(time=time)
-    exporter.UpdatePipeline(time)
+    # -----------------------------------------------------------------------------
+    # Perform analysis
+    # -----------------------------------------------------------------------------
+    analysis.begin()
 
-analysis.end()
+    for t in file_times:
+        time = int(t)
+        GetAnimationScene().TimeKeeper.Time = float(time)
+        fng.update_active_arguments(time=time)
+        print 'Generating images for timestep ',time
+        exporter.UpdatePipeline(time)
+
+    analysis.end()
+
+
+if __name__ == "__main__":
+    description = "Python script to generate dynamic rendering data for cam5"
+
+    parser = argparse.ArgumentParser(description=description)
+
+    parser.add_argument("--tsteps", type=str, default="", help="Comma-separated list of time steps to process")
+    parser.add_argument("--outdir", type=str, default="", help="Directory to write output")
+
+    args = parser.parse_args()
+
+    timesteps = args.tsteps.split(',')
+
+    print 'You want to process the following timesteps: ',timesteps
+
+    doProcessing(args.outdir, timesteps)
